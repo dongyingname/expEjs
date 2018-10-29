5dc66cbe9e046d42c9684b4b55a083a6513e9ef6
Ying Dong <dongyingname@yahoo.com>
2018-10-29 00:00:58 +0000

    finished server.js which is the app

--- server.js
+++ server.js
@@ -3,15 +3,63 @@ var express = require('express');
 var routes = require('./routes/index.js');
 var port = 3000;
 
+//
+const posts = [
+  {
+    id: 1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id: 2,
+    author: 'Drake',
+    title: 'Express: Starting from the Bottom',
+    body: 'Blog post number 2'
+  },
+  {
+    id: 3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 3'
+  },
+  {
+    id: 4,
+    author: 'Cody',
+    title: 'Events',
+    body: 'Blog post number 4'
+  }
+];
+
 var app = express();
 
 // Setup express application
 
 app.set('view engine', 'ejs');
-app.use('/public', express.static(process.cwd() + '/public'));
+
+
+// blog home page
+app.get('/', (req, res) => {
+  // render `home.ejs` with the list of posts
+  res.render('home', { posts: posts });
+});
+
+// blog post
+app.get('/post/:id', (req, res) => {
+  // find the post in the `posts` array
+  const post = posts.filter((post) => {
+    return post.id == req.params.id;
+  })[0];
+
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  });
+});
 
 //setup routes
-routes(app);
+//routes(app);
 //start server
 app.listen(port, function () {
   console.log("Server is running on "+ port +" port");
